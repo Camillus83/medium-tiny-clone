30acd126c61891e248011c0776c837c77744dab0
Camillus83 <kamilrichter98@gmail.com>
2022-10-10 21:54:25 +0200

    project configuration

--- medium_clone_project/settings.py
+++ medium_clone_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # LOCAL
     "accounts.apps.AccountsConfig",
+    "articles.apps.ArticlesConfig",
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = "medium_clone_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
